c5845d15d26de7747c316ca3e28d7f392494a4f2
Techno-420 <54204064+Techno-420@users.noreply.github.com>
2020-03-05 20:07:01 +0200

    Update 1
    HELOOOO UPDATEEEE

--- LAB.py
+++ LAB.py
@@ -1,1 +1,18 @@
 import csv
+def printmatr(matr):
+    for i in range(len(matr)):
+        print()
+        for el in matr[i]:
+            print( el, end=" ")
+countries = []
+with open('eurovision1.csv') as f:
+    read = csv.reader(f)
+    for row in read:
+        countries.append(row)
+with open('eurovision2.csv') as f:
+    read1 = csv.reader(f)
+    for row in read1:
+        countries.append(row)
+del countries[0]
+del countries[10]
+printmatr(countries)
