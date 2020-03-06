ab49056b6247bb7a6f95cf21f31e0b48b03aac59
Techno-420 <54204064+Techno-420@users.noreply.github.com>
2020-03-06 15:42:38 +0200

    UPD666
    So close to release

--- LAB.py
+++ LAB.py
@@ -10,20 +10,35 @@ def printmatr(matr):
 
 
 countries = []
-lst = os.listdir("D:\eurovis")
+name = str(input("Введите путь к папке:"))
+lst = os.listdir(name)
 for i in range(len(lst)):
-    os.chdir(r"D:\eurovis")
+    os.chdir(name)
     with open(lst[i]) as file:
         read = csv.reader(file)
         for row in read:
             countries.append(row)
-
 del countries[0]
 del countries[10]
-#printmatr(countries)
 for i in range(len(countries)):
     for j in range(1, len(countries[i])):
         countries[i][j] = int(countries[i][j])
+
+
+def points_and_countries(matrcountr):
+    pointcountrmass = []
+    for j in range(len(matrcountr)):
+        pointcountrmass.append([])
+    for i in range(len(pointcountrmass)):
+        for j in range(1):
+            pointcountrmass[i].append(matrcountr[i][j])
+            pointcountrmass[i].append(0)
+    return pointcountrmass
+pointscountr = points_and_countries(countries)
+
+
+
+printmatr(countries)
 
 
 def voice_of_day(matrcountr):
@@ -41,15 +56,31 @@ def sort_voice_of_day(matrvoice):
         matrvoice[i].sort(reverse=True)
     return matrvoice
 
-def points_and_countries(matrcountr):
-    pointcountrmass = []
-    for j in range(len(matrcountr)-1):
-        pointcountrmass.append([])
-    for i in range(len(pointcountrmass)):
-        for j in range(1):
-            pointcountrmass[i].append(matrcountr[i][j])
-            pointcountrmass[i].append([])
-    return pointcountrmass
-print(points_and_countries(countries))
+
+sortmatrx = sort_voice_of_day(voice_of_day(countries))
 
 
+def count_points(matrcountr, pointsandcountr, sortmatr):
+    point = 10
+    maxel = 0
+    for l in range(10):
+        for i in range(1, len(matrcountr)+1):
+            maxelindI = 0
+            maxel = sortmatr[i-1][l]
+            for k in range(1, len(matrcountr)+1):
+                for o in range(len(matrcountr)):
+                    if matrcountr[o][k] == maxel:
+                        matrcountr[o][k] = 0
+                        maxelindI = o
+            if l == 0:
+                pointsandcountr[maxelindI][1] += 12
+            elif l == 1:
+                pointsandcountr[maxelindI][1] += 10
+            elif l>=2 and l<=9:
+                pointsandcountr[maxelindI][1] += point
+        point -= 1
+    return pointsandcountr
+
+
+pointscountr = count_points(countries, pointscountr, sortmatrx)
+printmatr(pointscountr)
